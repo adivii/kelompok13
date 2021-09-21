360376d32e4bea0ccfa8bd83c12ced3da8626aa4
Irfan Saputra <75194907+irfanshow@users.noreply.github.com>
2021-09-21 07:31:46 +0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.kelompok;
+
+/**
+ *
+ * @author Asus-FX505DY
+ */
+public abstract class Employee {
+    private String name;
+    private String afm;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAfm() {
+        return afm;
+    }
+
+    public void setAfm(String afm) {
+        this.afm = afm;
+    }
+
+    abstract int payment();
+
+}
